3c72f2a569ad82cea8581ab7a6521d6be1977954
Zachary Smith <info@zachis.it>
2017-04-27 11:18:42 -0400

    adding sample CPT struture in functions.php

--- functions.php
+++ functions.php
@@ -96,4 +96,93 @@ function is_tree($pid) {      // $pid = The ID of the page we're looking for pag
         return false;  // we're elsewhere
 };
 
+/*************************************************************
+ * # Example CPT
+ *************************************************************/
+//register the CPT
+function wpt_event_posttype() {
+    register_post_type( 'events',
+        array(
+            'labels' => array(
+                'name' => __( 'Events' ),
+                'singular_name' => __( 'Event' ),
+                'add_new' => __( 'Add New Event' ),
+                'add_new_item' => __( 'Add New Event' ),
+                'edit_item' => __( 'Edit Event' ),
+                'new_item' => __( 'Add New Event' ),
+                'view_item' => __( 'View Event' ),
+                'search_items' => __( 'Search Event' ),
+                'not_found' => __( 'No events found' ),
+                'not_found_in_trash' => __( 'No events found in trash' )
+            ),
+            'public' => true,
+            'supports' => array( 'title', 'thumbnail' ),
+            'capability_type' => 'post',
+            'rewrite' => array("slug" => "events"), // Permalinks format
+            'menu_position' => 5,
+            'register_meta_box_cb' => 'add_events_metaboxes'
+        )
+    );
+}
+
+add_action( 'init', 'wpt_event_posttype' );
+
+// Add the Events Meta Boxes
+
+function add_events_metaboxes() {
+    add_meta_box('wpt_events_location', 'Event Location', 'wpt_events_location', 'events', 'side', 'default');
+}
+
+// The Event Location Metabox
+
+function wpt_events_location() {
+    global $post;
+
+    // Noncename needed to verify where the data originated
+    echo '<input type="hidden" name="eventmeta_noncename" id="eventmeta_noncename" value="' .
+        wp_create_nonce( plugin_basename(__FILE__) ) . '" />';
+
+    // Get the location data if its already been entered
+    $location = get_post_meta($post->ID, '_location', true);
+
+    // Echo out the field
+    echo '<input type="text" name="_location" value="' . $location  . '" class="widefat" />';
+
+}
+
+// Save the Metabox Data
+
+function wpt_save_events_meta($post_id, $post) {
+
+    // verify this came from the our screen and with proper authorization,
+    // because save_post can be triggered at other times
+    if ( !wp_verify_nonce( $_POST['eventmeta_noncename'], plugin_basename(__FILE__) )) {
+        return $post->ID;
+    }
+
+    // Is the user allowed to edit the post or page?
+    if ( !current_user_can( 'edit_post', $post->ID ))
+        return $post->ID;
+
+    // OK, we're authenticated: we need to find and save the data
+    // We'll put it into an array to make it easier to loop though.
+
+    $events_meta['_location'] = $_POST['_location'];
+
+    // Add values of $events_meta as custom fields
+
+    foreach ($events_meta as $key => $value) { // Cycle through the $events_meta array!
+        if( $post->post_type == 'revision' ) return; // Don't store custom data twice
+        $value = implode(',', (array)$value); // If $value is an array, make it a CSV (unlikely)
+        if(get_post_meta($post->ID, $key, FALSE)) { // If the custom field already has a value
+            update_post_meta($post->ID, $key, $value);
+        } else { // If the custom field doesn't have a value
+            add_post_meta($post->ID, $key, $value);
+        }
+        if(!$value) delete_post_meta($post->ID, $key); // Delete if blank
+    }
+
+}
+
+add_action('save_post', 'wpt_save_events_meta', 1, 2); // save the custom fields
 ?>
