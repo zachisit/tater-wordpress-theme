c62d2fb7859c8913afffee66a8f7cab30838248d
Zachary Smith <info@zachis.it>
2017-08-08 15:10:07 -0400

    updating metabox template with a required rubric

--- templates/metabox/EXAMPLE_metabox.template.php
+++ templates/metabox/EXAMPLE_metabox.template.php
@@ -7,6 +7,8 @@
  */
 ?>
 <div class="metabox">
+    <div id="required_explainer">Denotes a required field for input</div>
+
     <label for="_XXX">Button Button Text</label>
     <div class="subtitle_italic">Check below if you want the slider image to be black and white. Optional.</div>
     <input type="text" name="_XXX" value="<?= $_XXX[0] ?>" class="widefat" />
