f0f50efb2cf427a53aa4594c29e95789e25d17ba
Zachary Smith <info@zachis.it>
2017-08-08 14:04:12 -0400

    sample metabox template file

---
+++ templates/metabox/EXAMPLE_metabox.template.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Template for XXX Meta Box
+ * @var string $_XXX
+ *
+ */
+?>
+<div class="metabox">
+    <label for="_XXX">Button Button Text</label>
+    <div class="subtitle_italic">Check below if you want the slider image to be black and white. Optional.</div>
+    <input type="text" name="_XXX" value="<?= $_XXX[0] ?>" class="widefat" />
+</div>
