8d89b205e73b88392147baa050a6f19cf684b1a2
Zachary Smith <info@zachis.it>
2017-08-29 13:12:04 -0400

    updated mobile menu

--- header.php
+++ header.php
@@ -13,9 +13,12 @@
     <div id="logo">
         <a href="<?php echo get_home_url(); ?>" title="Home"><img src="<?php echo get_template_directory_uri(); ?>/images/preload/logo.png" alt="<?php echo get_bloginfo( 'name' ); ?> - Home" /></a>
     </div>
+    <button id="menu_btn"></button>
     <div id="menu">
-        <a href="javascript:void(0);" id="menu_btn"><div class="mobilemenubars"></div><div class="mobilemenubars"></div><div class="mobilemenubars"></div></a>
-        <div id="menu"><a href="#" id="menu_close">X</a>
-            <?php wp_nav_menu( [ 'theme_location' => 'header_menu', 'menu_id' => 'primary-menu' ] ); ?>
+        <button id="menu_close"></button>
+        <div id="search_mobile">
+            <?php get_search_form(); ?>
+        </div>
+        <?php wp_nav_menu( [ 'theme_location' => 'header_menu', 'menu_id' => 'primary-menu' ] ); ?>
     </div>
 </header>
