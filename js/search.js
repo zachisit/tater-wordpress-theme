e609c46e80d3a1744effac0fd3b22a1b6d26c987
Zachary Smith <info@zachis.it>
2018-01-23 16:16:50 -0500

    updated search.js to include blur/focus events

--- js/search.js
+++ js/search.js
@@ -1,27 +1,40 @@
 /**
  * File search.js
  *
- * if user hits search button with nothing filled in:
- * redirect focus to input field
+ * 1. general onBlur and onFocus events
  *
- * @version: 1.0
- * @package: tater
+ // * 2. if user hits search button with nothing filled in:
+ // * redirect focus to input field
+ *
+ * @package tater
+ * @version 1.0.0
  */
-
 jQuery(document).ready(function($) {
+    var $globalSearchBlurClass = $('.searchBlur');
 
-    $('.search-submit').click(function(e){
-        var $searchField = $('.search-field'),
-            $searchFieldValue = $searchField.val(),
-            hoverColor = '#ffff66';
-
-        console.log($searchField);
-        console.log($searchFieldValue);
-
-        if ($searchFieldValue === '') {
-            e.preventDefault();
-            $searchField.css('background', hoverColor);
-            $searchField.get(0).focus();
+    $globalSearchBlurClass.blur(function() {
+        if (this.value === '') {
+            this.value = 'Enter a keyword';
         }
-    })
+    });
+    $globalSearchBlurClass.focus(function() {
+        if (this.value === 'Enter a keyword') {
+            this.value = '';
+        }
+    });
+
+    // $('.search-submit').click(function(e){
+    //     var $searchField = $('.search-field'),
+    //         $searchFieldValue = $searchField.val(),
+    //         hoverColor = '#ffff66';
+    //
+    //     console.log($searchField);
+    //     console.log($searchFieldValue);
+    //
+    //     if ($searchFieldValue === '') {
+    //         e.preventDefault();
+    //         $searchField.css('background', hoverColor);
+    //         $searchField.get(0).focus();
+    //     }
+    // })
 });
